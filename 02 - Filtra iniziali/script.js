9c8549b711e3fca6d9e191d816e70fa42b9b74a7
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-04-25 13:53:11 +0200

    feat: filtra iniziali completed

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const filtro = "A"
 
 // Dichiara la funzione qui.
+function filtraPerIniziale(array, lettera) {
+    const risultato = [];
 
+    for (let i = 0; i < array.length; i++) {
+        const parola = array[i];
+        if (parola[0] === lettera) {
+            risultato.push(parola);
+        }
+    }
+    return risultato;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const risultato = filtraPerIniziale(names, filtro);
+console.log(risultato);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
