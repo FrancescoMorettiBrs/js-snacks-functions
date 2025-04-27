d0672b6b38b4961c356a98972b44aa661ebe4611
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-04-27 14:13:42 +0200

    feat: saluti completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,12 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluta(nome) {
+    return `Ciao, ${nome}!`;
+  }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(saluta(name))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
